c4f24cfd4791cff5316db5693e15fbccee8cc0ce
gdpancheng <gdpancheng@gmail.com>
2014-05-22 14:17:09 +0800

    修复了一个低级错误

--- src/com/android/pc/ioc/image/ImageDownloader.java
+++ src/com/android/pc/ioc/image/ImageDownloader.java
@@ -18,7 +18,6 @@ import android.widget.Toast;
 
 import com.android.pc.ioc.app.ApplicationBean;
 import com.android.pc.util.Handler_System;
-import com.wash.activity.R;
 
 /**
  * 图片下载类 可以设置图片高度宽度
@@ -48,7 +47,7 @@ public class ImageDownloader extends ImageResizer {
 		super(context, imageSize);
 		init(context);
 	}
-
+	
 	public ImageDownloader(Context context) {
 		super(context, Handler_System.getDisplayMetrics().get(Handler_System.systemWidth));
 		init(context);
@@ -56,7 +55,6 @@ public class ImageDownloader extends ImageResizer {
 
 	/**
 	 * 初始化缓存 和 检查网络
-	 * 
 	 * @author gdpancheng@gmail.com 2014-5-19 下午2:55:49
 	 * @param context
 	 * @return void
@@ -64,6 +62,7 @@ public class ImageDownloader extends ImageResizer {
 	private void init(Context context) {
 		checkConnection(context);
 	}
+
 
 	/**
 	 * 简单的网络判断
@@ -74,7 +73,7 @@ public class ImageDownloader extends ImageResizer {
 		final ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 		final NetworkInfo networkInfo = cm.getActiveNetworkInfo();
 		if (networkInfo == null || !networkInfo.isConnectedOrConnecting()) {
-			Toast.makeText(context, R.string.no_network_connection_toast, Toast.LENGTH_LONG).show();
+			Toast.makeText(context, "无法链接到网络", Toast.LENGTH_LONG).show();
 			ApplicationBean.logger.e("网络连接失败");
 		}
 	}
@@ -86,43 +85,43 @@ public class ImageDownloader extends ImageResizer {
 	 *            普通的url链接
 	 * @return 返回调整过大小的bitmap
 	 */
-	private Bitmap processBitmap(String data, ImageView imageView) {
-
+	private Bitmap processBitmap(String data,ImageView imageView) {
+		
 		ApplicationBean.logger.d("图片下载开始 - " + data);
 		File file = null;
-		// 网络图片 本地SD卡图片
+		//网络图片 本地SD卡图片
 		if (data.startsWith("http")) {
 			final String key = ImageCache.hashKeyForDisk(data);
 			file = ImageCache.getFromFileCache(key);
 			start(imageView);
-			downloadUrlToStream(data, file, imageView);
-		} else {
-			file = new File(data);
+			downloadUrlToStream(data, file,imageView);
+        }else {
+        	file = new File(data);
 		}
-
+		
 		Bitmap bitmap = null;
 		if (file != null && file.exists()) {
 			bitmap = decodeSampledBitmapFromFile(file.getPath(), mImageWidth, mImageHeight, getImageCache());
 		}
-		if (bitmap == null) {
+		if (bitmap==null) {
 			fail(imageView);
-		} else {
-			finish(bitmap, imageView);
+        }else {
+        	finish(bitmap, imageView);
 		}
 		return bitmap;
 	}
 
 	@Override
-	protected Bitmap processBitmap(Object data, ImageView imageView) {
-		// 本地sdcard和网络
+	protected Bitmap processBitmap(Object data,ImageView imageView) {
+		//本地sdcard和网络
 		if (data.getClass() == String.class) {
-			return processBitmap(String.valueOf(data), imageView);
-		}
-		// draw文件
+			return processBitmap(String.valueOf(data),imageView);
+        }
+		//draw文件
 		if (data.getClass() == Integer.class) {
-			return super.processBitmap(Integer.valueOf(data.toString()), imageView);
-		}
-		return processBitmap(String.valueOf(data), imageView);
+			return super.processBitmap(Integer.valueOf(data.toString()),imageView);
+        }
+		return processBitmap(String.valueOf(data),imageView);
 	}
 
 	/**
@@ -132,7 +131,7 @@ public class ImageDownloader extends ImageResizer {
 	 *            获取的url
 	 * @return 成功则返回true 否则返回false
 	 */
-	public boolean downloadUrlToStream(String urlString, File file, ImageView imageView) {
+	public boolean downloadUrlToStream(String urlString, File file,ImageView imageView) {
 		disableConnectionReuseIfNecessary();
 		HttpURLConnection urlConnection = null;
 		InputStream in = null;
@@ -154,10 +153,10 @@ public class ImageDownloader extends ImageResizer {
 				}
 				out.write(bytes, 0, count);
 				n = n + count;
-				if (length > 0) {
+				if (length>0) {
 					int percent = (int) (n * 100 / length);
-					process(percent, imageView);
-				}
+					process(percent,imageView);
+                }
 			}
 			return true;
 		} catch (final IOException e) {
